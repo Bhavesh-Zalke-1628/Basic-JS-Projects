e97409d84c5e66097254fdf22d4d667d0f9db02b
Bhavesh Zalke <125982261+Bhavesh-Zalke-1628@users.noreply.github.com>
2023-09-09 19:21:23 +0530

    1.1

--- changeColor/script.js
+++ changeColor/script.js
@@ -1,5 +1,28 @@
-var container = document.querySelector("#container")
+const rect = document.querySelector("#container")
 
-container.addEventListener("click",function(){
-    container.style.backgroundColor="red"
+rect.addEventListener("mousemove",function(details){
+    var rectLocation = rect.getBoundingClientRect()
+    var insideRectVal=(details.clientX - rectLocation.left)
+    if(insideRectVal < rectLocation.width / 2){
+        var redColor = gsap.utils.mapRange(0,rectLocation.width/2,255,0,insideRectVal)
+        gsap.to(rect,{
+            backgroundColor : `rgb(${redColor},0,0)`,
+            ease : Power4,
+            duration :1
+        })
+    }else{
+        var blueColor = gsap.utils.mapRange(rectLocation.width/2,rectLocation.width,0,255,insideRectVal)
+        gsap.to(rect,{
+            backgroundColor : `rgb(0,0,${blueColor})`,
+            ease : Power4,
+            duration :1
+        })
+        // console.log('right')
+    }
 })
+
+rect.addEventListener('mouseleave',function(){
+    gsap.to(rect,{
+        backgroundColor :" white"
+    });
+});
