ea348c7f06e50df67f29498a193cab04ab32feed
Bhavesh Zalke <zalkebhavesh@gmail.com>
2023-09-17 09:07:24 +0530

    1.1

---
+++ Pop up imgae/script.js
@@ -0,0 +1,28 @@
+     // Throttling Function
+const throttleFunction=(func, delay)=>{
+ 
+  // Previously called time of the function
+  let prev = 0;
+  return (...args) => {
+    // Current called time of the function
+    let now = new Date().getTime();
+ 
+    // Logging the difference between previously
+    // called and current called timings
+    console.log(now-prev, delay);
+     
+    // If difference is greater than delay call
+    // the function again.
+    if(now - prev> delay){
+      prev = now;
+ 
+      // "..." is the spread operator here
+      // returning the function with the
+      // array of arguments
+      return func(...args); 
+    }
+  }
+}
+document.querySelector("#center").addEventListener("mousemove", throttleFunction(()=>{
+  console.log("chala")
+}, 500));
